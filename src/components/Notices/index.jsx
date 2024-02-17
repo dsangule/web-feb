9ca2b3a8e31af16f3b47231671ea3ae4a531d181
Anurudh Singh <anurudhsingh1010@gmail.com>
2024-02-17 13:32:11 +0530

    created notices page

--- src/components/Notices/index.jsx
+++ src/components/Notices/index.jsx
@@ -3,9 +3,114 @@ import Header from '../Header'
 
 function Notices() {
   return (
-    <div>
+    <div className='bg-[#b7becf] pb-[100px]'>
       <Header />
-      Notices
+      <h1 className="font-oswald" style={{
+        fontSize: "45px",
+        color: '#011F4B',
+        marginLeft: '75px',
+        marginTop: "28px",
+      }}>
+        Notices
+      </h1>
+      <div style={{
+        marginLeft: '98px',
+        backgroundColor: "#D9D9D9",
+        width: "90%",
+        height: "41px",
+        marginTop: "28px",
+        paddingLeft: "10px",
+        paddingTop: "8px"
+
+      }}>
+        <p>
+          first line
+
+        </p>
+      </div>
+      <div style={{
+        marginLeft: '98px',
+        backgroundColor: "#D9D9D9",
+        width: "90%",
+        height: "41px",
+        marginTop: "28px",
+        paddingLeft: "10px",
+        paddingTop: "8px"
+      }}>
+        <p>
+          second line
+
+        </p>
+      </div>
+      <div style={{
+        marginLeft: '98px',
+        backgroundColor: "#D9D9D9",
+        width: "90%",
+        height: "41px",
+        marginTop: "28px",
+        paddingLeft: "10px",
+        paddingTop: "8px",
+      }}>
+        <p>
+          third line
+
+        </p>
+
+      </div>
+      <h2 className="font-oswald" style={{
+        fontSize: "45px",
+        color: '#011F4B',
+        marginLeft: "75px",
+        marginTop: "100px",
+      }}>
+        Events
+      </h2>
+      <div style={{
+
+        marginLeft: '98px',
+        backgroundColor: "#D9D9D9",
+        width: "90%",
+        height: "100px",
+        marginTop: "28px",
+        paddingLeft: "10px",
+        paddingTop: "8px"
+      }}>
+        <p>
+          first line
+
+        </p>
+      </div>
+      <div style={{
+        marginLeft: '98px',
+        backgroundColor: "#D9D9D9",
+        width: "90%",
+        height: "100px",
+        marginTop: "28px",
+        paddingLeft: "10px",
+        paddingTop: "8px"
+      }}>
+        <p>
+          second line
+
+        </p>
+
+      </div>
+      <div style={{
+        marginLeft: '98px',
+        backgroundColor: "#D9D9D9",
+        width: "90%",
+        height: "100px",
+        marginTop: "28px",
+        paddingLeft: "10px",
+        paddingTop: "8px"
+
+      }}>
+        <p>
+          third line
+
+        </p>
+
+      </div>
     </div>
   )
 }
