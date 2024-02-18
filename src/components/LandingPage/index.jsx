48c701a1f06dbcfd0810621aecec6f70c1f5bcad
Abhishek Singh Kushwah <abhishek.kushwah.0007@gmail.com>
2024-02-18 12:24:17 +0530

    landing page button

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Button } from "antd";
+import { Button, Flex } from "antd";
 import Header from "../Header";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import styles from './landingpage.module.css';
 
 function LandingPage() {
   const router = useRouter();
@@ -16,14 +17,18 @@ function LandingPage() {
       </div>
       <Image src={'/JNU-says-LP.svg'} width={500} height={500} className="align-middle m-auto max-w-[80%]"/>
       <center>
-        RLord bohot bada bakchod hai<br/>
-        <Button type="default" className="bg-gray-500" onClick={()=>{router.push('/login')}}>Login</Button>
+        <div className={`${styles.wrapper}`}>
+        
+        <Button type="submit" shape="round" className=" h-10 w-40 font-bold font-arimo  " onClick={()=>{router.push('/login')}}>LOGIN</Button>
+        
+        </div>
       </center>
       <div className="bg-gradient-to-b from-[#878866a0] to-[#545744a0] h-24 w-full fixed bottom-0">
         <div className="flex my-auto mx-auto pt-7 w-fit max-w-[50vw] justify-center">
           <Image src={'/icons/LinkedIn.svg'} width={50} height={50} className="mx-4 max-w-[20%]" />
           <Image src={'/icons/Instagram.svg'} width={50} height={50} className="mx-4 max-w-[20%]" />
           <Image src={'/icons/Discord.svg'} width={50} height={50} className="mx-4 max-w-[20%]" />
+        
         </div>
       </div>
     </div>
